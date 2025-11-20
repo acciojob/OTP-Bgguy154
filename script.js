d05f97040ad3ff71cf053344a0d0130518c44c44
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2025-11-20 18:01:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const inputs=document.queryselectorAll(".code");
+
+inputs[0].focus();
+inputs.forEach((input,index)=>{
+	 input.addEventListener('input',(e)=>{
+		 e.preventDefault();
+		 const value=e.target.value;
+		 if(value && index<inputs.length-1){
+			 inputs[index+1].focus();
+		 }
+	 })
+
+	input.addEventListener("keydown",(e)=>{
+		e.preventDefault();
+		if(e.key=="backspace"){
+			if(input.value==""){
+				if(index>0){
+					inputs[index-1].focus();
+					inputs[index-1]="";
+				}
+			}
+			else{
+				input.value="";
+			}
+		}
+	})
+})
